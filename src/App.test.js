24c5044f3fdc5b3ac2d34ee8924419a0a956ef52
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-07-14 19:59:59 -0500

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({adapter: new EnzymeAdapter() });
+
+const setup = (props={}, state=null) => {
+    return shallow(<App {...props}/>)
+}
+
+const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test="${val}"]`);
+}
+
+test('renders without error', () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, 'component-app')
+    expect(appComponent.length).toBe(1);
 });
+
+test('renders increment button', () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, 'increment-button')
+    expect(appComponent.length).toBe(1);
+})
+
+test('renders counter display', () =>{
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, 'counter-display')
+    expect(appComponent.length).toBe(1);
+})
+
+test('counter starts at 0', () => {
+
+})
+
+test('clicking button increments counter display', () => {
+
+})
